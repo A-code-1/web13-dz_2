a50a7e69a85181a1df543ea72ac09269bf029713
Antonina Andreeva <antonina@Antoninas-MacBook-Air.local>
2025-05-16 23:54:19 +0300

    Обновлены файлы: работает страница одного вопроса, пагинация)

--- app/views.py
+++ app/views.py
@@ -41,22 +41,27 @@ def index(request):
 def hot(request):
     page_num = int(request.GET.get('page', 1))
     paginator = Paginator(QUESTIONS, 5)
-    page = paginator.page(page_num)
+    try:
+        page = paginator.page(page_num)
+    except PageNotAnInteger:
+        page = paginator.page(1)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
     return render(request, template_name='hot.html', context={'questions': page.object_list, 'page': page})
 
 def question(request, question_id):
-    try:
-        question = QUESTIONS[question_id]  
-    except IndexError:
-       raise Http404("Question not found")
-    return render(request, 'single_question.html', {'question': question})
-
-def answer(request, answer_id):
     page_num = request.GET.get('page', 1)
     paginator = Paginator(ANSWERS, 5)
-    try:
-        answer = ANSWERS[answer_id]  
-    except IndexError:
-       raise Http404("Answer not found")
-    return render(request, 'single_question.html', {'answer': answer})
 
+    try:
+        page = paginator.page(page_num)
+    except PageNotAnInteger:
+        page = paginator.page(1)
+    except EmptyPage:
+        page = paginator.page(paginator.num_pages)
+
+    try:
+        question = ANSWERS[question_id]  
+    except IndexError:
+       raise Http404("Question not found")
+    return render(request, 'single_question.html', {'question': question, 'answers': page.object_list, 'page': page})
